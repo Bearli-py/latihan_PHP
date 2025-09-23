8aa11e80f02378c84421a2d3e8cb1ded8a418dd5
Bearli-py <firamaksalina@gmail.com>
2025-09-23 14:17:31 +0700

    latihan_PHP

---
+++ .history/string_20250923141033.php
@@ -0,0 +1,25 @@
+<?php 
+$a = "Hello "; 
+$b = "World!" 
+$a .= $b; 
+echo $a; // Hello World! 
+echo "<br />"; 
+$a = "belajar "; 
+$b = "PHP "; 
+$c = "di Politeknik Negeri Jember"; 
+$hasil= "Saya sedang ".$a.$b.$c; 
+echo $hasil; // Maria sedang belajar PHP di Politeknik Negeri Jember 
+?>
+
+#Alternatif
+<?php 
+$a = "Hello "; 
+$hasil = "{$a} World!"; 
+echo $hasil; // Hello World! 
+echo "<br />"; 
+$a = "belajar "; 
+$b = "PHP "; 
+$c = "di Politeknik Negeri Jember"; 
+$hasil= "Saya sedang {$a}{$b}{$c}"; 
+echo $hasil; // Maria sedang belajar PHP di Politeknik Negeri Jember 
+?>
